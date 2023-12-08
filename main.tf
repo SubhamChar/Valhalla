749fdc6d42fa125578c5217080f134c85189ce81
Subham Char <45358861+SubhamChar@users.noreply.github.com>
2023-12-09 00:20:45 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"  # Change to your preferred region
+}
+
+resource "aws_vpc" "valhalla_vpc" {
+  cidr_block = "10.0.0.0/16"
+  # Other VPC configurations
+}
+
+resource "aws_instance" "valhalla_instance" {
+  ami           = "ami-12345678"  # Change to the appropriate AMI
+  instance_type = "t2.micro"      # Change to your preferred instance type
+  # Other instance configurations
+}
+
+# Other AWS resources (security groups, key pair, etc.)
